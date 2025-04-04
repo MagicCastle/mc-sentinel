c81dfed598327c642bbcf97514300e209282069c
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2025-04-04 08:52:31 -0400

    update: added very basic prometheus reporting

--- mc_sentinel/main.py
+++ mc_sentinel/main.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, Response, request
+import shutil
+
+from prometheus_client import CONTENT_TYPE_LATEST, Counter, generate_latest
 
 
 app = Flask(__name__)
+
+puppet_status_request = Counter(
+    "puppet_status_request",
+    "Records the number of status request for puppet",
+    labelnames=["state"],
+)
+
+
+def get_puppet_version():
+    if shutil.which("puppet"):
+        puppet_status_request.labels(state="success").inc()
+    else:
+        puppet_status_request.labels(state="failure").inc()
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
+
+
+@app.route("/metrics")
+def metrics():
+    """
+    This is the exporter that generate a report based of the latest metrics.
+    """
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+
+@app.route("/manual-recording")
+def man_recording():
+    """
+    Endpoint to trigger the manual collection of metrics by Collectors.
+    """
+    get_puppet_version()
+    return Response("Manuel recording succeeded")
