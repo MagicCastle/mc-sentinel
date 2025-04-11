3bddbe3a64ec639d8e860edad438f0d4bf3ff3e0
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2025-04-11 12:05:12 -0400

    regroup logics into methods and document with docstrings

--- mc_sentinel/collector.py
+++ mc_sentinel/collector.py
@@ -1,5 +1,6 @@
-from prometheus_client.registry import Collector, REGISTRY
+from prometheus_client.registry import Collector
 from prometheus_client.core import CounterMetricFamily, GaugeMetricFamily
+from typing import Union
 import os
 import requests
 
@@ -8,54 +9,107 @@ class SentinelCollector(Collector):
     def __init__(self):
         super().__init__()
 
-    def collect(self):
-        """
-        This method is called by the Prometheus client to collect metrics.
-        """
-        # Metrics
-        puppet_core_install_directory = CounterMetricFamily(
-            "puppet_core_install_directory",
-            "Install directory found on machine",
-        )
-        puppetserver_port_poll_success = CounterMetricFamily(
-            "puppetserver_port_polls_success",
-            "Successful poll on puppetserver's port",
-        )
-        puppetserver_port_poll_failure = CounterMetricFamily(
-            "puppetserver_port_polls_failure",
-            "Failed poll on puppetserver's port",
-        )
-        dummy_metric = GaugeMetricFamily(
-            "dummy_metric",
-            "Dummy metric for testing purposes",
-        )
+    def _status_add_metric(
+        self,
+        is_success: bool,
+        success_metric: Union[CounterMetricFamily, GaugeMetricFamily],
+        fail_metric: Union[CounterMetricFamily, GaugeMetricFamily],
+    ):
+        """Add opposing metrics based on the success value.
 
-        # Add the dummy metric to the collector
-        dummy_metric.add_metric([], 1)
-
-        # Collection
-        if os.path.exists("/opt/puppetlabs/bin/puppet"):
-            puppet_core_install_directory.add_metric([], 1)
+        Args:
+            is_success (bool): Indicates if the operation was successful.
+            success_metric (Union[CounterMetricFamily, GaugeMetricFamily]): Metric to add if successful.
+            fail_metric (Union[CounterMetricFamily, GaugeMetricFamily]): Metric to add if failed.
+        """
+        if is_success:
+            success_metric.add_metric([], 1)
+            fail_metric.add_metric([], 0)
         else:
-            puppet_core_install_directory.add_metric([], 0)
+            success_metric.add_metric([], 0)
+            fail_metric.add_metric([], 1)
 
-        # Check if the puppet server is running and accessible
-        try:
-            puppetserver_status = requests.get(
-                "https://mgmt1:8140/status/v1/simple",
-                verify=False,
+    def collect(self):
+        """Collect metrics from the Puppet server.
+
+        Yields:
+            Union[CounterMetricFamily, GaugeMetricFamily]: Metrics to be collected.
+        """
+
+        def dummy_metric():
+            """Creates a dummy metric for testing purposes.
+
+            Returns:
+                GaugeMetricFamily: A dummy metric for testing purposes.
+            """
+            dummy_metric = GaugeMetricFamily(
+                "dummy_metric",
+                "Dummy metric for testing purposes",
             )
-            if puppetserver_status.status_code == 200:
-                puppetserver_port_poll_success.add_metric([], 1)
-                puppetserver_port_poll_failure.add_metric([], 0)
-            if puppetserver_status.status_code == 503:
-                puppetserver_port_poll_success.add_metric([], 0)
-                puppetserver_port_poll_failure.add_metric([], 1)
-        except requests.exceptions.RequestException:
-            puppetserver_port_poll_success.add_metric([], 0)
-            puppetserver_port_poll_failure.add_metric([], 1)
 
-        yield puppet_core_install_directory
-        yield puppetserver_port_poll_success
-        yield puppetserver_port_poll_failure
-        yield dummy_metric
+            dummy_metric.add_metric([], 1)
+            return dummy_metric
+
+        def puppet_core_install_directory():
+            """Checks if the Puppet core install directory exists.
+
+            Returns:
+                CounterMetricFamily: A metric indicating the presence of the Puppet core install directory.
+            """
+
+            puppet_core_install_directory = CounterMetricFamily(
+                "puppet_core_install_directory",
+                "Install directory found on machine",
+            )
+            if os.path.exists("/opt/puppetlabs/bin/puppet"):
+                puppet_core_install_directory.add_metric([], 1)
+            else:
+                puppet_core_install_directory.add_metric([], 0)
+            return puppet_core_install_directory
+
+        def puppetserver_port_poll():
+            """Polls the Puppet server's port and check for success.
+
+            Attempts to connect to the Puppet server's port and checks if the connection is successful. It uses the requests library to make a GET request to the Puppet server's status endpoint. If the request is successful (HTTP status code 200), it indicates that the port is open and accessible. Otherwise, it indicates a failure.
+
+            Returns:
+                tuple: A tuple containing two CounterMetricFamily objects:
+                    - puppetserver_port_poll_success: Counter for successful polls.
+                    - puppetserver_port_poll_failure: Counter for failed polls.
+            """
+            puppetserver_port_poll_success = CounterMetricFamily(
+                "puppetserver_port_polls_success",
+                "Successful poll on puppetserver's port",
+            )
+            puppetserver_port_poll_failure = CounterMetricFamily(
+                "puppetserver_port_polls_failure",
+                "Failed poll on puppetserver's port",
+            )
+            try:
+                puppetserver_response = requests.get(
+                    "https://mgmt1:8140/status/v1/simple",
+                    verify=False,
+                )
+                if puppetserver_response.status_code == 200:
+                    self._status_add_metric(
+                        True,
+                        puppetserver_port_poll_success,
+                        puppetserver_port_poll_failure,
+                    )
+                else:
+                    self._status_add_metric(
+                        False,
+                        puppetserver_port_poll_success,
+                        puppetserver_port_poll_failure,
+                    )
+            except requests.exceptions.RequestException:
+                self._status_add_metric(
+                    False,
+                    puppetserver_port_poll_success,
+                    puppetserver_port_poll_failure,
+                )
+            return puppetserver_port_poll_success, puppetserver_port_poll_failure
+
+        yield puppet_core_install_directory()
+        yield from puppetserver_port_poll()
+        yield dummy_metric()
